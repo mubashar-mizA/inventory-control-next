764eec27fb60fd2a514cb1dd4eacdb6f2f9198ba
mubashar-mizA <mirzamubashar535@gmail.com>
2025-04-25 06:53:46 +0500

    adding dashboard top bar

---
+++ app/components/DashboardTopNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardTopNav = ({ toggleSidebar }) => {
+  return (
+    <div className="flex items-center justify-between p-4 border-b">
+      <div className="text-lg font-bold">Inventory Control System</div>
+    </div>
+  );
+};
+
+export default DashboardTopNav;
