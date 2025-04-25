545cdd8d8d349e91771801ca4b3f4ea16043c4ad
mubashar-mizA <mirzamubashar535@gmail.com>
2025-04-25 22:24:16 +0500

    updating app/components/DashboardTopNav.jsx

--- app/components/DashboardTopNav.jsx
+++ app/components/DashboardTopNav.jsx
@@ -3,14 +3,8 @@ import { Menu } from "lucide-react";
 
 const DashboardTopNav = ({ toggleSidebar }) => {
   return (
-    <div className="flex items-center justify-between p-4 border-b">
+    <div className="flex items-center justify-between border-b bg-gray-800 text-white py-4 fixed top-0 w-full z-50 px-6">
       {/* Menu button - visible only on small screens */}
-      <button
-        onClick={toggleSidebar}
-        className="md:hidden text-black"
-      >
-        <Menu size={28} />
-      </button>
 
       <div className="text-lg font-bold">Inventory Control System</div>
     </div>
