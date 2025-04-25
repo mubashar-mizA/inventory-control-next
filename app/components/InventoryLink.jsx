e684f224eeb7bb700f3e24e145e52b4ec0cfa043
mubashar-mizA <mirzamubashar535@gmail.com>
2025-04-25 22:25:00 +0500

    updating  app/components/InventoryLink.jsx

--- app/components/InventoryLink.jsx
+++ app/components/InventoryLink.jsx
@@ -2,8 +2,11 @@ import Link from "next/link";
 
 export default function InventoryLink({ linkTxt, linkUrl, className }) {
   return (
-    <Link href={linkUrl} className={`text-sm font-medium ${className}`}>
+    <Link
+      href={linkUrl}
+      className={`text-sm font-bold text-gray-400  ${className}`}>
       {linkTxt}
+
     </Link>
   );
 }
