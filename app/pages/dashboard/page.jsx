e221be058cadcd229167be346cfcb40728f99f5d
mubashar-mizA <mirzamubashar535@gmail.com>
2025-04-25 22:26:22 +0500

    updating app/pages/dashboard/page.jsx

--- app/pages/dashboard/page.jsx
+++ app/pages/dashboard/page.jsx
@@ -2,14 +2,20 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="space-y-6">
-      <h1 className="text-2xl font-bold ">Dashboard</h1>
-      <p className="">
+    <div className="mt-10">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <p>
         Welcome to your dashboard. Here you can view your profile, manage your
         inventory, and more.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus
+        officiis eos molestias amet nihil. Maxime facilis provident accusamus
+        fugit, optio, nesciunt vitae rerum!
+      </p>
+      <p>
         
       </p>
-
     </div>
   );
 };
