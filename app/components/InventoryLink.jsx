3913690ff9ef01e32b0b063885e4b521b3ecaa95
mubashar-mizA <mirzamubashar535@gmail.com>
2025-04-26 22:15:28 +0500

    updating  app/components/InventoryLink.jsx

--- app/components/InventoryLink.jsx
+++ app/components/InventoryLink.jsx
@@ -1,11 +1,23 @@
 import Link from "next/link";
 
-export default function InventoryLink({ linkTxt, linkUrl, className }) {
+export default function InventoryLink({ linkTxt,
+  linkUrl,
+  className,
+  icon,
+  isCollapsed }) {
   return (
     <Link
+
       href={linkUrl}
-      className={`text-sm font-bold text-gray-400  ${className}`}>
-      {linkTxt}
+
+      className={`text-lg  
+
+      ${className}`}>
+
+      <div className={` flex gap-2 ${isCollapsed ? 'flex-col text-sm items-center juc' : ''}`}>
+        {icon}
+        {linkTxt}
+      </div>
 
     </Link>
   );
