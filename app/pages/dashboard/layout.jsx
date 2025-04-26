ca2f5f189bd5a37a278ebf758a18b0e920e82868
mubashar-mizA <mirzamubashar535@gmail.com>
2025-04-26 22:16:27 +0500

    updating app/pages/dashboard/layout.jsx

--- app/pages/dashboard/layout.jsx
+++ app/pages/dashboard/layout.jsx
@@ -5,30 +5,52 @@ import DashboardTopNav from "@/app/components/DashboardTopNav";
 import React, { useState } from "react";
 
 const Layout = ({ children }) => {
-  const [isCollapsed, setIsCollapsed] = useState(true); // Sidebar collapsed by default on desktop
+
+  const [isCollapsed, setIsCollapsed] = useState(false); // Sidebar collapsed by default on desktop
 
   const toggleSidebar = () => {
     setIsCollapsed(!isCollapsed);
   };
 
   return (
-    <div className="flex min-h-screen bg-gray-100">
+    <div
+      className="flex min-h-screen bg-gray-100"
+    >
+
       {/* Sidebar */}
-      <DashboardSideNav isCollapsed={isCollapsed} toggleSidebar={toggleSidebar} />
+
+      <DashboardSideNav
+        isCollapsed={isCollapsed}
+        toggleSidebar={toggleSidebar}
+      />
 
       {/* Main Content */}
+
       <div
-        className={`flex-1 transition-all duration-300 ${
-          isCollapsed ? "md:ml-16" : "md:ml-44"
-        } w-full`}
+        className={`flex-1 transition-all duration-300 
+          ${isCollapsed ? "md:ml-28" : "md:ml-56"
+          } w-full`
+        }
       >
+
         {/* Top Navigation */}
-        <div className="sticky top-0 z-20">
+
+        <div
+          className="sticky top-0 z-20"
+        >
           <DashboardTopNav />
         </div>
+
         {/* Child Content */}
-        <main className="p-4 md:p-6">{children}</main>
+
+        <main
+          className="p-4 md:p-6 mt-8"
+        >
+          {children}
+        </main>
+
       </div>
+
     </div>
   );
 };
