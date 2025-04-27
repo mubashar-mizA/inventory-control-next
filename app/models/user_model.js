ec251617354139cae648a262d606917538a3ca3c
mubashar-mizA <mirzamubashar535@gmail.com>
2025-04-27 21:33:28 +0500

    adding app/models/

---
+++ app/models/user_model.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  name: { type: String, trim: true, required: true },
+  email: { type: String, trim: true, required: true },
+  password: { type: String, trim: true, required: true },
+});
+export const userModel =
+  mongoose.models.users || mongoose.model("users", userSchema);
