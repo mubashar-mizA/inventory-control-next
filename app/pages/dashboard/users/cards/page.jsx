b50ee180c16bf6bc42fe3b18225ea600c4fb3c6b
mubashar-mizA <mirzamubashar535@gmail.com>
2025-04-27 21:31:14 +0500

    adding app/pages/dashboard/users/

---
+++ app/pages/dashboard/users/cards/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserTable from '@/app/components/UserTable'
+const UserCards = () => {
+  return (
+    <div>
+      <UserTable />
+      {/*  */}
+    </div>
+  )
+}
+
+export default UserCards
---
+++ app/pages/dashboard/users/edite/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditeUser = () => {
+    return (
+        <div>EditeUser</div>
+    )
+}
+
+export default EditeUser
