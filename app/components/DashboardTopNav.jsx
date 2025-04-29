d522ed24c99fb68d00cc171966d0b63f98d0e8a7
mubashar-mizA <mirzamubashar535@gmail.com>
2025-04-29 22:05:55 +0500

    updating  app/components/DashboardTopNav.jsx

--- app/components/DashboardTopNav.jsx
+++ app/components/DashboardTopNav.jsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { Menu } from "lucide-react";
+import { useSession } from "next-auth/react";
 
 const DashboardTopNav = ({ toggleSidebar }) => {
-  return (
-    <div
-      className="flex items-center justify-between border-b py-5 fixed top-0 w-full z-50 px-6 bg-white">
-      {/* Menu button - visible only on small screens */}
+  const { data } = useSession();
+  let userName = (data?.user?.name)
+  userName = userName?.toLocaleUpperCase()
 
-      <div className="text-lg font-bold">Inventory Control System</div>
+  return (
+    <div className="fixed top-0 z-50 w-full border-b bg-white py-5">
+
+      <div className="mx-auto flex  flex-col px-4 sm:px-6">
+
+        <h1 className="text-lg font-bold ">Inventory Control System</h1>
+
+        <h2 className="text-sm text-gray-700 ">
+          Welcome back {userName || "Loading..."}
+        </h2>
+
+      </div>
     </div>
   );
 };
