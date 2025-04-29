da919d91f5c0a59f1b2e8512195edc980a6da1f7
mubashar-mizA <mirzamubashar535@gmail.com>
2025-04-29 11:43:41 +0500

    updating the  app/pages/dashboard/users/cards/page.jsx

--- app/pages/dashboard/users/cards/page.jsx
+++ app/pages/dashboard/users/cards/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import UserTable from '@/app/components/UserTable'
+import UserDrawer from '@/app/components/UserDrawer'
+
 const UserCards = () => {
   return (
     <div>
       <UserTable />
-      {/*  */}
+      <UserDrawer />
     </div>
   )
 }
