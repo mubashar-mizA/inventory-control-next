530ca9d2829d7094d5d74f0325496d604220364d
mubashar-mizA <mirzamubashar535@gmail.com>
2025-04-30 17:40:17 +0500

    updating  app/models/user_model.js

--- app/models/user_model.js
+++ app/models/user_model.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+
 const userSchema = new mongoose.Schema({
   name: { type: String, trim: true, required: true },
-  email: { type: String, trim: true, required: true },
+  email: { type: String, trim: true, required: true, unique: true },
   password: { type: String, trim: true, required: true },
+  otp: { type: String }, // temporary OTP
+  isVerified: { type: Boolean, default: false }, // user must verify OTP
 });
+
 export const userModel =
   mongoose.models.users || mongoose.model("users", userSchema);
