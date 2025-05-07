64ff64cd29dcf08e79f2ad3f19f2492ff749f53b
mubashar-mizA <mirzamubashar535@gmail.com>
2025-05-07 18:52:54 +0500

    removing unnecessary classes

--- app/components/MyAccordian.jsx
+++ app/components/MyAccordian.jsx
@@ -16,54 +16,57 @@ export default function MyAccordion({
     setIsOpen(!isOpen);
   };
 
-  
-
   return (
-    <div>
+    <div className="this-is-main-div-of-accordian-component flex flex-col gap-2">
+
       <button
         onClick={toggleAccordion}
-        className={`flex rounded-lg transition-colors w-full text-lg ${
-          isCollapsed && !isMobileOpen ? "justify-center" : "items-center gap-2"
-        }`}
+        className={`flex this-is-button-very-next-to-div
+          ${isCollapsed && !isMobileOpen ? " when-navbar-collapsed" : " when-navbar-not-collapsed"
+          }`}
         aria-expanded={isOpen}
         aria-label={`Toggle ${mainTitle} accordion`}
       >
+
         <div
-          className={`flex gap-2 ${
-            isCollapsed && !isMobileOpen
-              ? "flex-col text-sm items-center justify-center"
-              : ""
-          }`}
+          className={`flex  this-is-very-next-div-inside--main-div-of-accordian-component ${isCollapsed && !isMobileOpen
+            ? "flex-col text-sm "
+            : ""
+            }`}
         >
           {icon}
           <span className="flex-1">{mainTitle}</span>
         </div>
+
         {/* Show chevron only when not collapsed or in mobile open mode */}
+
         {(!isCollapsed || isMobileOpen) && (
           <>{isOpen ? <ChevronDown size={20} /> : <ChevronRight size={20} />}</>
         )}
+
       </button>
 
+
       {(!isCollapsed || isMobileOpen) && isOpen && (
-        <div className="pl-8 flex flex-col gap-1">
+        <div className="flex flex-col gap-2">
           {items.map((item, index) => (
             <a
               key={index}
               href={item.link || "#"}
-              className="flex items-center gap-2 p-2 rounded-md text-sm"
+              className="flex text-sm"
             >
               <div
-                className={`flex gap-2 ${
-                  isCollapsed ? "flex-col text-sm items-center justify-center" : ""
-                }`}
+                className={`flex gap-2 ml-4 ${isCollapsed ? "flex-col text-sm" : ""
+                  }`}
               >
                 {item.icon}
-                <span className="text-black">{item.name}</span>
+                <span className="text-gray-900">{item.name}</span>
               </div>
             </a>
           ))}
         </div>
       )}
+
     </div>
   );
 }
