c2d4bb1706eb54a3b47641bd450056ee3702af26
mubashar-mizA <mirzamubashar535@gmail.com>
2025-05-07 18:51:59 +0500

    making the top section of dashboard responsive

--- app/components/DashboardTopNav.jsx
+++ app/components/DashboardTopNav.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import { useSession } from "next-auth/react";
+import { Avatar } from "@heroui/react";
 
-const DashboardTopNav = ({ toggleSidebar }) => {
+const DashboardTopNav = ({ toggleSidebar, className }) => {
   const { data } = useSession();
   console.log('Data ====>*', data)
   let userName = (data?.user?.name)
   userName = userName?.toLocaleUpperCase()
 
-  return (
-    <div className="fixed top-0 z-50 w-full border-b bg-white py-5">
+  return <section
+    className={`this-is-in-top-nav-bar-component p-5 border-b shadow-sm
+      ${className} 
+      flex justify-between items-center`}
+  >
+    <h2 className="text-gray-500 font-bold">Inventory Control System </h2>
+    <Avatar
+      name="Joe"
+      size="md"
+      className="border border-blue-800 hidden sm:flex sm:items-center sm:justify-center"
+    />
+  </section>
 
-      <div className="mx-auto flex  flex-col px-4 sm:px-6">
-
-        <h1 className="text-lg font-bold ">Inventory Control System</h1>
-
-        <h2 className="text-sm text-gray-700 ">
-          Welcome back {userName || "Loading..."}
-        </h2>
-
-      </div>
-    </div>
-  );
 };
 
 export default DashboardTopNav;
